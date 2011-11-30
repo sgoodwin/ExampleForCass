5ba1e17026afb403bd8655ae95a88fa8830dbf58
Samuel Goodwin <goodwinlabs@gmail.com>
2011-11-30 18:55:22 -0500

    Initial Commit

---
+++ ExampleForCass/GOAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GOAppDelegate.h
+//  ExampleForCass
+//
+//  Created by Samuel Goodwin on 11/30/11.
+//  Copyright (c) 2011 SNAP Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GOViewController;
+
+@interface GOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) GOViewController *viewController;
+
+@end
---
+++ ExampleForCass/GOViewController.h
@@ -0,0 +1,13 @@
+//
+//  GOViewController.h
+//  ExampleForCass
+//
+//  Created by Samuel Goodwin on 11/30/11.
+//  Copyright (c) 2011 SNAP Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GOViewController : UIViewController
+
+@end
